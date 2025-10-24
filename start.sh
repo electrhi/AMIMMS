d1b4f79a5013bed49c6d3095d6456185ffca7554
electrhi <electrichi3882@gmail.com>
2025-10-24 13:06:15 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 echo "🚀 Starting Gunicorn..."
-export PATH="$PATH:/opt/render/project/src/.venv/bin"
-exec gunicorn --workers=2 --bind 0.0.0.0:$PORT app:app
+
+# Render 환경에서 가상환경 경로를 명시적으로 추가
+export PATH="/opt/render/project/src/.venv/bin:$PATH"
+
+# gunicorn 실행
+exec /opt/render/project/src/.venv/bin/gunicorn --workers=2 --bind 0.0.0.0:$PORT app:app
