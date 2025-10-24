997e4accf4daeec3bb71a9ee1f9605c8ce04bef5
electrhi <electrichi3882@gmail.com>
2025-10-24 13:00:54 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+pip install --upgrade pip setuptools wheel
+pip install Pillow==10.2.0
+
+
 echo "🔧 Installing dependencies..."
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
