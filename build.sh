decfaae500fd9c9e35d5bd6a5168621afb5bd684
electrhi <electrichi3882@gmail.com>
2025-10-28 17:24:33 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ pip install --upgrade pip setuptools wheel
 pip install --force-reinstall Pillow==11.0.0
 
 # 나머지 주요 패키지
-pip install Flask==3.0.3 gunicorn==22.0.0 qrcode==7.4.2 openpyxl==3.1.2 pandas==2.2.3 google-auth==2.35.0 gspread==6.1.2 google-api-python-client==2.147.0 google-cloud-storage==2.18.2 google-auth-httplib2==0.2.0 requests==2.32.5 protobuf==6.33.0 certifi==2025.10.5
+pip install Flask==3.0.3 gunicorn==22.0.0 qrcode==7.4.2 openpyxl==3.1.2 pandas==2.2.3 google-auth==2.35.0 gspread==6.1.2 google-api-python-client==2.147.0 google-cloud-storage==2.18.2 google-auth-httplib2==0.2.0 requests==2.32.3 protobuf==6.33.0 certifi==2025.10.5 urllib3==2.2.3
 
 
 echo "✅ Build completed."
